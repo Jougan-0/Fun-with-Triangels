bdf273ece7ced88fd8f1a6058298772b90a0ead2
Shlok Mishra <99207534+Jougan-0@users.noreply.github.com>
2023-01-19 15:33:35 +0530

    Feat:the files

---
+++ main.js
@@ -0,0 +1,25 @@
+const InputAngels=document.querySelectorAll(".Angels");
+const checkbutton=document.querySelector("#check-button");
+const OutputBox=document.querySelector("#Output-Box");
+
+
+checkbutton.addEventListener("click",function CheckIfTriangel(){
+    for(let i=0;i<3;i++)
+    if(InputAngels[i].value===""||InputAngels[i].value==="0")
+    {
+        OutputBox.innerText="Please enter all the fields or one of them is zero";
+        return;
+    }
+    var sum=TheTotalsum();
+    if(sum===180)
+    OutputBox.innerText="YEppe it's a triangel😃";
+    else
+    OutputBox.innerText="Sorry not a triangle 😥";
+});
+
+function TheTotalsum(){
+    var sum=0;
+    for(let i=0;i<3;i++)
+    sum=sum+Number(InputAngels[i].value);
+    return sum;
+}
